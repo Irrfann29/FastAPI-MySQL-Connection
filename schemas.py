116ba75bf94cd3fdd3f24da224d2e6e8d30f685d
Irrfann29 <mrirfanhashmi2303@gmail.com>
2026-01-20 14:42:26 +0530

    Minor changes

--- schemas.py
+++ schemas.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
     name: str
-    email: EmailStr  # Validates that the string is a proper email format
+    email: EmailStr 
