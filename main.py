493e833555a5f91d0bd9aeaf3cbaba3b69f8b345
Irrfann29 <mrirfanhashmi2303@gmail.com>
2026-01-20 14:41:09 +0530

    Minor Changes

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from database import SessionLocal, engine
 
 app = FastAPI()
 
-# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
@@ -15,16 +14,13 @@ def get_db():
 
 @app.post("/users/", response_model=schemas.UserCreate)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # 1. Create a model instance from the Pydantic data
     new_user = models.User(name=user.name, email=user.email)
     
-    # 2. Add the object to the session
+    
     db.add(new_user)
     
-    # 3. Commit the transaction to save to MySQL
     db.commit()
     
-    # 4. Refresh to get the generated ID from the database
     db.refresh(new_user)
     
     return new_user
